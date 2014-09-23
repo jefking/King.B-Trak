c2a5cee06cfa25979a61a3689fabfd2fc4f1bf3b
Jef King <jef@agilebusinesscloud.com>
2014-09-23 16:24:54 -0700

    Program Structure

--- King.BTrak.Program/Program.cs
+++ King.BTrak.Program/Program.cs
@@ -1,9 +1,30 @@
 ﻿namespace King.BTrak.Program
 {
+    using System;
+    using System.Diagnostics;
+
+    /// <summary>
+    /// B-Trak synchronizer, from SQL Server to Azure Table Storage
+    /// </summary>
     public class Program
     {
+        #region Methods
+        /// <summary>
+        /// Program Main Entry
+        /// </summary>
+        /// <param name="args">Program Arguments</param>
         public static void Main(string[] args)
         {
+            try
+            {
+            }
+            catch (Exception ex)
+            {
+                Trace.Fail(ex.Message, ex.StackTrace);
+            }
+
+            Trace.TraceInformation("Completed.");
         }
+        #endregion
     }
 }
