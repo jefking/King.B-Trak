eed1fdc0eb35f18ca2bc4bda7409262b37c71aca
Jef King <jef@agilebusinesscloud.com>
2014-09-24 19:24:28 -0700

    added integration test case project

---
+++ King.BTrak.Integration.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("King.BTrak.Integration.Test")]
+[assembly: AssemblyDescription("Integration Tests")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("King.BTrak.Integration.Test")]
+[assembly: AssemblyCopyright("Copyright © Jef King 2014")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("ed24d90b-2f31-434e-b28c-4aecaa98c904")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
