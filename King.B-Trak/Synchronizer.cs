f117f2b9dc90964de932d8b81bd676f0a6690bfd
Jef King <jef@agilebusinesscloud.com>
2014-09-26 21:00:04 -0700

    more tracing

--- King.B-Trak/Synchronizer.cs
+++ King.B-Trak/Synchronizer.cs
@@ -1,6 +1,7 @@
 ﻿namespace King.BTrak
 {
     using King.Azure.Data;
+    using System;
     using System.Data.SqlClient;
     using System.Diagnostics;
     using System.Threading.Tasks;
@@ -24,6 +25,11 @@
         /// <param name="config"></param>
         public Synchronizer(IConfigValues config)
         {
+            if (null == config)
+            {
+                throw new ArgumentNullException("config");
+            }
+
             this.config = config;
         }
         #endregion
@@ -54,6 +60,15 @@
         public ISynchronizer Run()
         {
             Trace.TraceInformation("Running...");
+
+            Trace.TraceInformation("Loading Database Schema.");
+            Trace.TraceInformation("Loaded Database Schema.");
+
+            Trace.TraceInformation("Loading SQL Server Data.");
+            Trace.TraceInformation("Loaded SQL Server Data.");
+
+            Trace.TraceInformation("Storing SQL Server Data.");
+            Trace.TraceInformation("Stored SQL Server Data.");
 
             Trace.TraceInformation("Ran.");
 
