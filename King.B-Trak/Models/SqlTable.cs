5ff38e3c894a3c56c51e5dcf0aa10bb290779dc0
Jef King <jef@agilebusinesscloud.com>
2014-10-11 13:51:00 -0700

    Creating Table, loading data

---
+++ King.B-Trak/Models/SqlTable.cs
@@ -0,0 +1,49 @@
+﻿namespace King.BTrak.Models
+{
+    using System;
+
+    public class SqlTable
+    {
+        public Guid Id
+        {
+            get;
+            set;
+        }
+
+        public string TableName
+        {
+            get;
+            set;
+        }
+
+        public string PartitionKey
+        {
+            get;
+            set;
+        }
+
+        public string RowKey
+        {
+            get;
+            set;
+        }
+
+        public string ETag
+        {
+            get;
+            set;
+        }
+
+        public DateTime Timestamp
+        {
+            get;
+            set;
+        }
+
+        public string Data
+        {
+            get;
+            set;
+        }
+    }
+}
--- King.B-Trak/SqlDataWriter.cs
+++ King.B-Trak/SqlDataWriter.cs
@@ -1,6 +1,7 @@
 ﻿namespace King.BTrak
 {
     using King.BTrak.Models;
+    using King.Data.Sql.Reflection;
     using System;
     using System.Collections.Generic;
     using System.Linq;
@@ -11,20 +12,41 @@
     {
         public const string table = @"CREATE TABLE [btrak].[{0}]
                                     (
-                                        id uniqueidentifier
-                                        , data xml
+	                                    [Id] UNIQUEIDENTIFIER NOT NULL DEFAULT NEWID(), 
+                                        [TableName] NVARCHAR(255) NOT NULL, 
+                                        [PartitionKey] NVARCHAR(255) NOT NULL, 
+                                        [RowKey] NVARCHAR(255) NOT NULL, 
+                                        [ETag] NCHAR(255) NOT NULL, 
+                                        [Timestamp] DATETIME NOT NULL, 
+                                        [SynchronizedOn] DATETIME NOT NULL DEFAULT GETUTCDATE(),
+                                        [Data] XML NULL, 
+                                        PRIMARY KEY ([TableName], [RowKey], [PartitionKey])
                                     );";
 
         private readonly string tableName = null;
+
+        /// <summary>
+        /// Schema Reader
+        /// </summary>
+        protected readonly SchemaReader reader = null;
 
         public SqlDataWriter(IConfigValues config)
         {
             this.tableName = config.SqlTableName;
         }
 
-        public void CreateTable()
+        public async Task<bool> CreateTable()
         {
-            var tableStatement = string.Format(table, this.tableName);
+            var exists = (from t in await this.reader.Load(SchemaTypes.Table)
+                           where t.Name == this.tableName
+                           select true).FirstOrDefault();
+            if (!exists)
+            {
+                var tableStatement = string.Format(table, this.tableName);
+                return true;
+            }
+
+            return false;
         }
 
         public void Store(IDictionary<string, object> data)
