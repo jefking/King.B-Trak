20667ed6655acf277c46d8f16997fdd46a0ca648
Jef King <jef@agilebusinesscloud.com>
2014-10-11 14:44:21 -0700

    Merge Statement

--- King.BTrak.Schema/btrak/Sprocs/SaveTableData.sql
+++ King.BTrak.Schema/btrak/Sprocs/SaveTableData.sql
@@ -4,12 +4,67 @@
     , @PartitionKey NVARCHAR(255) = NULL
     , @RowKey NVARCHAR(255) = NULL
     , @ETag NCHAR(255) = NULL 
-    , @Timestamp DATETIME = NULL 
-    , @SynchronizedOn DATETIME = NULL
+    , @Timestamp DATETIME = NULL
     , @Data XML = NULL
 AS
 BEGIN
 
-	RETURN 0;
+	MERGE INTO [dbo].[TableData] [table]
+	USING
+	(
+		SELECT @Id
+		, @TableName
+		, @PartitionKey
+		, @RowKey
+		, @ETag
+		, @Timestamp
+		, @Data
+		, GETUTCDATE()
+	) AS [Data]
+	(
+		[Id]
+		, [TableName]
+		, [PartitionKey]
+		, [RowKey]
+		, [ETag]
+		, [Timestamp]
+		, [Data]
+		, [SynchronizedOn]
+	)
+	ON
+	(
+		[table].[TableName] = [data].[TableName]
+		AND [table].[PartitionKey] = [data].[PartitionKey]
+		AND [table].[RowKey] = [data].[RowKey]
+	)
+	WHEN MATCHED THEN
+		UPDATE SET [table].[Id] = [data].[Id]
+			, [table].[ETag] = [data].[ETag]
+			, [table].[Timestamp] = [data].[Timestamp]
+			, [table].[SynchronizedOn] = [data].[SynchronizedOn]
+			, [table].[Data] = [data].[Data]
+	WHEN NOT MATCHED THEN
+		INSERT
+		(
+			[Id]
+			, [TableName]
+			, [PartitionKey]
+			, [RowKey]
+			, [ETag]
+			, [Timestamp]
+			, [SynchronizedOn]
+			, [Data]
+		)
+		VALUES
+		(
+			[data].[Id]
+			, [data].[TableName]
+			, [data].[PartitionKey]
+			, [data].[RowKey]
+			, [data].[ETag]
+			, [data].[Timestamp]
+			, [data].[SynchronizedOn]
+			, [data].[Data]
+		);
 
 END
