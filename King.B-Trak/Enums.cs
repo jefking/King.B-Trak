9b9f28ec98c9b4225bd74bf0d1bd1f9c41c6d316
Jef King <jef@agilebusinesscloud.com>
2014-10-12 10:52:32 -0700

    Model Test

--- King.B-Trak/Enums.cs
+++ King.B-Trak/Enums.cs
@@ -1,9 +1,14 @@
 ﻿namespace King.BTrak
 {
+    #region Direction
+    /// <summary>
+    /// Syncronization Direction
+    /// </summary>
     public enum Direction : byte
     {
         Unknown = 0,
         SqlToTable = 1,
         TableToSql = 2,
     }
+    #endregion
 }
--- King.BTrak.Unit.Test/Models/ConfigValuesTests.cs
+++ King.BTrak.Unit.Test/Models/ConfigValuesTests.cs
@@ -1,5 +1,6 @@
 ﻿namespace King.BTrak.Unit.Test.Models
 {
+    using King.BTrak.Models;
     using NUnit.Framework;
     using System;
     using System.Collections.Generic;
@@ -10,5 +11,76 @@
     [TestFixture]
     public class ConfigValuesTests
     {
+        [Test]
+        public void Constructor()
+        {
+            new ConfigValues();
+        }
+
+        [Test]
+        public void IsIConfigValues()
+        {
+            Assert.IsNotNull(new ConfigValues() as IConfigValues);
+        }
+
+        [Test]
+        public void SqlConnection()
+        {
+            var expected = Guid.NewGuid().ToString();
+            var c = new ConfigValues
+            {
+                SqlConenction = expected,
+            };
+
+            Assert.AreEqual(expected, c.SqlConenction);
+        }
+
+        [Test]
+        public void StorageAccountConnection()
+        {
+            var expected = Guid.NewGuid().ToString();
+            var c = new ConfigValues
+            {
+                StorageAccountConnection = expected,
+            };
+
+            Assert.AreEqual(expected, c.StorageAccountConnection);
+        }
+
+        [Test]
+        public void StorageTableName()
+        {
+            var expected = Guid.NewGuid().ToString();
+            var c = new ConfigValues
+            {
+                StorageTableName = expected,
+            };
+
+            Assert.AreEqual(expected, c.StorageTableName);
+        }
+
+        [Test]
+        public void SqlTableName()
+        {
+            var expected = Guid.NewGuid().ToString();
+            var c = new ConfigValues
+            {
+                SqlTableName = expected,
+            };
+
+            Assert.AreEqual(expected, c.SqlTableName);
+        }
+
+        [Test]
+        public void SyncDirection()
+        {
+            var expected = Direction.SqlToTable;
+            var c = new ConfigValues
+            {
+                SyncDirection = expected,
+            };
+
+            Assert.AreEqual(expected, c.SyncDirection);
+        }
     }
 }
