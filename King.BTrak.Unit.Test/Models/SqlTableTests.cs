f36b6954b916271d15dae07a34e12e9ac6d4d7d5
Jef King <jef@agilebusinesscloud.com>
2014-10-12 10:55:29 -0700

    test cases

--- King.BTrak.Unit.Test/Models/SqlTableTests.cs
+++ King.BTrak.Unit.Test/Models/SqlTableTests.cs
@@ -1,14 +1,29 @@
 ﻿namespace King.BTrak.Unit.Test.Models
 {
+    using King.BTrak.Models;
+    using King.Mapper.Generated.Sql;
     using NUnit.Framework;
-    using System;
-    using System.Collections.Generic;
-    using System.Linq;
-    using System.Text;
-    using System.Threading.Tasks;
 
     [TestFixture]
     public class SqlTableTests
     {
+        [Test]
+        public void Constructor()
+        {
+            new SqlTable();
+        }
+
+        [Test]
+        public void IsdboSaveTableData()
+        {
+            Assert.IsNotNull(new SqlTable() as dboSaveTableData);
+        }
+
+        [Test]
+        public void FullyQualifiedName()
+        {
+            var st = new SqlTable();
+            Assert.AreEqual("[dbo].[SaveTableData]", st.FullyQualifiedName());
+        }
     }
 }
