22c20b04d6f76455b5aef73998b1556b33e77c6d
Jef King <jef@agilebusinesscloud.com>
2014-10-12 10:53:51 -0700

    Model Test

--- King.BTrak.Unit.Test/Models/SqlDataTests.cs
+++ King.BTrak.Unit.Test/Models/SqlDataTests.cs
@@ -1,14 +1,41 @@
 ﻿namespace King.BTrak.Unit.Test.Models
 {
+    using King.BTrak.Models;
     using NUnit.Framework;
     using System;
     using System.Collections.Generic;
-    using System.Linq;
-    using System.Text;
-    using System.Threading.Tasks;
 
     [TestFixture]
     public class SqlDataTests
     {
+        [Test]
+        public void Constructor()
+        {
+            new SqlData();
+        }
+
+        [Test]
+        public void TableName()
+        {
+            var expected = Guid.NewGuid().ToString();
+            var c = new SqlData
+            {
+                TableName = expected,
+            };
+
+            Assert.AreEqual(expected, c.TableName);
+        }
+
+        [Test]
+        public void Rows()
+        {
+            var expected = new List<IDictionary<string, object>>();
+            var c = new SqlData
+            {
+                Rows = expected,
+            };
+
+            Assert.AreEqual(expected, c.Rows);
+        }
     }
 }
