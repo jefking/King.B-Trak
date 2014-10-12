8562000aaa3a1726bd65e19e4d0a547e80054259
Jef King <jef@agilebusinesscloud.com>
2014-10-12 12:02:07 -0700

    Test Cases

--- King.BTrak.Unit.Test/TableStorageReaderTests.cs
+++ King.BTrak.Unit.Test/TableStorageReaderTests.cs
@@ -1,14 +1,43 @@
 ﻿namespace King.BTrak.Unit.Test
 {
+    using King.Azure.Data;
+    using NSubstitute;
     using NUnit.Framework;
     using System;
-    using System.Collections.Generic;
-    using System.Linq;
-    using System.Text;
-    using System.Threading.Tasks;
 
     [TestFixture]
     public class TableStorageReaderTests
     {
+        [Test]
+        public void Constructor()
+        {
+            var resources = Substitute.For<IAzureStorageResources>();
+            var tableName = Guid.NewGuid().ToString();
+            new TableStorageReader(resources, tableName);
+        }
+
+        [Test]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void ConstructorStorageResourcesNull()
+        {
+            var tableName = Guid.NewGuid().ToString();
+            new TableStorageReader(null, tableName);
+        }
+
+        [Test]
+        [ExpectedException(typeof(ArgumentException))]
+        public void ConstructorTableNameNull()
+        {
+            var resources = Substitute.For<IAzureStorageResources>();
+            new TableStorageReader(resources, null);
+        }
+
+        [Test]
+        public void IsITableStorageReader()
+        {
+            var resources = Substitute.For<IAzureStorageResources>();
+            var tableName = Guid.NewGuid().ToString();
+            Assert.IsNotNull(new TableStorageReader(resources, tableName) as ITableStorageReader);
+        }
     }
 }
