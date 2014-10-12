0f3daf5c889b60a8776b2d9f15bc4c80e453e74d
Jef King <jef@agilebusinesscloud.com>
2014-10-12 12:00:08 -0700

    test cases

--- King.BTrak.Unit.Test/TableStorageWriterTests.cs
+++ King.BTrak.Unit.Test/TableStorageWriterTests.cs
@@ -1,14 +1,32 @@
 ﻿namespace King.BTrak.Unit.Test
 {
+    using King.Azure.Data;
+    using NSubstitute;
     using NUnit.Framework;
     using System;
-    using System.Collections.Generic;
-    using System.Linq;
-    using System.Text;
-    using System.Threading.Tasks;
 
     [TestFixture]
     public class TableStorageWriterTests
     {
+        [Test]
+        public void Constructor()
+        {
+            var table = Substitute.For<ITableStorage>();
+            new TableStorageWriter(table);
+        }
+
+        [Test]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void ConstructorTableNull()
+        {
+            new TableStorageWriter(null);
+        }
+
+        [Test]
+        public void IsITableStorageWriter()
+        {
+            var table = Substitute.For<ITableStorage>();
+            Assert.IsNotNull(new TableStorageWriter(table) as ITableStorageWriter);
+        }
     }
 }
