ed85e58c28759e7854a8f77d80192f8bdc2cbea4
Jef King <jef@agilebusinesscloud.com>
2014-10-12 11:14:42 -0700

    Constructor Tests

--- King.BTrak.Unit.Test/SynchronizerTests.cs
+++ King.BTrak.Unit.Test/SynchronizerTests.cs
@@ -1,14 +1,32 @@
 ﻿namespace King.BTrak.Unit.Test
 {
+    using King.BTrak.Models;
     using NUnit.Framework;
     using System;
-    using System.Collections.Generic;
-    using System.Linq;
-    using System.Text;
-    using System.Threading.Tasks;
 
     [TestFixture]
     public class SynchronizerTests
     {
+        [Test]
+        public void Constructor()
+        {
+            var c = new ConfigValues()
+            {
+                StorageAccountConnection = "UseDevelopmentStorage=true;",
+                SqlConenction = "Server=server;Database=database;Trusted_Connection=True;",
+                SqlTableName = "tabletable",
+                StorageTableName = "tabletable",
+                SyncDirection = Direction.TableToSql,
+            };
+
+            new Synchronizer(c);
+        }
+
+        [Test]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void ConstructorConfigNull()
+        {
+            new Synchronizer(null);
+        }
     }
 }
