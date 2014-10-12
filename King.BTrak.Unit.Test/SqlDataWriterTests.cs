f274edeafa1a9e2d442caf48f6fec748e51c7541
Jef King <jef@agilebusinesscloud.com>
2014-10-12 12:04:30 -0700

    Test Cases

--- King.BTrak.Unit.Test/SqlDataWriterTests.cs
+++ King.BTrak.Unit.Test/SqlDataWriterTests.cs
@@ -1,14 +1,48 @@
 ﻿namespace King.BTrak.Unit.Test
 {
+    using King.Data.Sql.Reflection;
+    using King.Mapper.Data;
+    using NSubstitute;
     using NUnit.Framework;
     using System;
-    using System.Collections.Generic;
-    using System.Linq;
-    using System.Text;
-    using System.Threading.Tasks;
 
     [TestFixture]
     public class SqlDataWriterTests
     {
+        [Test]
+        public void Constructor()
+        {
+            var reader = Substitute.For<ISchemaReader>();
+            var executor = Substitute.For<IExecutor>();
+            var tableName = Guid.NewGuid().ToString();
+            new SqlDataWriter(reader, executor, tableName);
+        }
+
+        [Test]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void ConstructorReaderNull()
+        {
+            var executor = Substitute.For<IExecutor>();
+            var tableName = Guid.NewGuid().ToString();
+            new SqlDataWriter(null, executor, tableName);
+        }
+
+        [Test]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void ConstructorExecutorNull()
+        {
+            var reader = Substitute.For<ISchemaReader>();
+            var tableName = Guid.NewGuid().ToString();
+            new SqlDataWriter(reader, null, tableName);
+        }
+
+        [Test]
+        [ExpectedException(typeof(ArgumentException))]
+        public void ConstructorTableNameNull()
+        {
+            var reader = Substitute.For<ISchemaReader>();
+            var executor = Substitute.For<IExecutor>();
+            new SqlDataWriter(reader, executor, null);
+        }
     }
 }
